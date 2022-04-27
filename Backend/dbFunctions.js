907026508ce0e8ea9b5ffbc1151f077408c83bbe
divyanshvn <190050035@iitb.ac.in>
2022-04-28 02:44:11 +0530

    Added some backend functionalities

--- Backend/dbFunctions.js
+++ Backend/dbFunctions.js
@@ -9,6 +9,8 @@ const res = require('express/lib/response');
 // const INFLUX_TOKEN = "vOFwtnaq3Op08NBRo3JmLg21oA4Xcj_NV7FMOsFuwVfJ35DPzU77FWMkTMYv7HPe1s-hCxh4ulPdEweSDHV0SA=="
 // const INFLUX_URL = "http://localhost:8086"
 
+let checks = {};
+
 const url = process.env.INFLUX_URL || ''
 const token = process.env.INFLUX_TOKEN
 const org = process.env.INFLUX_ORG || ''
@@ -16,10 +18,14 @@ const org = process.env.INFLUX_ORG || ''
 const queryApi = new InfluxDB({url,token}).getQueryApi(org)
 
 const graph_data = (req,res)=>{
-    
-    const fluxQuery = 'from(bucket: "MyBucket1")\
-    |> range(start: -10w)\
-    |> filter(fn: (r) => r["_measurement"] == "boltdb_reads_total")'
+    var metric = (req.params.metric);
+    var value = parseInt(req.params.value);
+    var time_start = req.params.time_start;
+    var time_end = req.params.time_end;
+
+    const fluxQuery = `from(bucket: "MyBucket1")\
+    |> range(start: ${time_start} ,stop: ${time_end} )\
+    |> filter(fn: (r) => r["_measurement"] == "${metric}")`
 
     // ret_obj = {}
     let x = []
@@ -27,13 +33,8 @@ const graph_data = (req,res)=>{
     queryApi.queryRows(fluxQuery, {
         next(row, tableMeta) {
             const o = tableMeta.toObject(row)
-            // console.log(o);
+
             x.push(o._value);
-            // for(var i = 0; i < 3; i++)
-            //     console.log(x[i]);
-            
-            // console.log("NEW");
-            // console.log(typeof(o._value));
             time_x.push(o._time);
         //   console.log(
         //     `${o._time} ${o._measurement} : ${o._field}=${o._value}`
@@ -49,13 +50,19 @@ const graph_data = (req,res)=>{
             console.log('\nFinished SUCCESS')
         },
     });
-    // console.log("NICE\n");
-    // for(var i = 0; i < 10; i++){
-    //     console.log(x[i]);
-    // }
     
+}
+
+const add_check = (req,res)=>{
+    var check_meas = req.body.measurement;
+    var check_val = parseInt(req.body.value);
+
+    checks[check_meas] = check_val;
+    var x = {status: "New Threshold check Added"};
+    res.send(x);
 }
 
 module.exports = {
     graph_data,
+    add_check,
 }
--- Backend/routing.js
+++ Backend/routing.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const dbFunctions = require('./dbFunctions')
 
-router.get('/graph/:metric/:feature', dbFunctions.graph_data)
+router.get('/graph/:metric/:value/:time_start/:time_end', dbFunctions.graph_data)
+router.post('/threshold_check/', dbFunctions.add_check)
 
 module.exports = router;
