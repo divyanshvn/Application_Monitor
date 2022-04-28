3041cfe83ada2e82200907e02ebf9bc8d7585fbb
divyanshvn <190050035@iitb.ac.in>
2022-04-28 17:13:34 +0530

    Backend For Graph

--- Backend/dbFunctions.js
+++ Backend/dbFunctions.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const {InfluxDB,Point} = require('@influxdata/influxdb-client');
-const {response,query} = require('express');
+const { InfluxDB, Point } = require('@influxdata/influxdb-client');
+const { response, query } = require('express');
 const res = require('express/lib/response');
 
 // const INFLUX_ORG = "MyDB"
@@ -12,57 +12,65 @@ const res = require('express/lib/response');
 let checks = {};
 
 const url = process.env.INFLUX_URL || ''
-const token = process.env.INFLUX_TOKEN
+const token = process.env.INFLUX_API_TOKEN
 const org = process.env.INFLUX_ORG || ''
 
-const queryApi = new InfluxDB({url,token}).getQueryApi(org)
+const queryApi = new InfluxDB({ url, token }).getQueryApi(org)
 
-const graph_data = (req,res)=>{
-    var metric = (req.params.metric);
-    var value = parseInt(req.params.value);
-    var time_start = req.params.time_start;
-    var time_end = req.params.time_end;
+const graph_data = (req, res) => {
+  var process = (req.params.process);
+  var metric = (req.params.metric);
+  var time_start = req.params.time_start;
+  var time_end = req.params.time_end;
 
-    const fluxQuery = `from(bucket: "MyBucket1")\
-    |> range(start: ${time_start} ,stop: ${time_end} )\
-    |> filter(fn: (r) => r["_measurement"] == "${metric}")`
+  const fluxQuery = `from(bucket: "InsertData1")\
+  |> range(start: ${time_start}, stop: ${time_end} )\
+  |> filter(fn: (r) => r["_measurement"] == "${process}")
+  |> filter(fn: (r) => r["_field"] == "${metric}")`
 
-    // ret_obj = {}
-    let x = []
-    let time_x = []
-    queryApi.queryRows(fluxQuery, {
-        next(row, tableMeta) {
-            const o = tableMeta.toObject(row)
+  // const fluxQuery = 'from(bucket: "MyBucket1")\
+  //   |> range(start: -10w, stop : -4m) \
+  //   |> filter(fn: (r) => r["_measurement"] == "boltdb_reads_total")'
 
-            x.push(o._value);
-            time_x.push(o._time);
-        //   console.log(
-        //     `${o._time} ${o._measurement} : ${o._field}=${o._value}`
-        //   )        
-            
-        },
-        error(error) {
-        console.error(error)
-          console.log('\nFinished ERROR')
-        },
-        complete() {
-            res.send({"time":time_x, "vals":x});
-            console.log('\nFinished SUCCESS')
-        },
-    });
-    
+  // ret_obj = {}
+  let x = []
+  let time_x = []
+  queryApi.queryRows(fluxQuery, {
+    next(row, tableMeta) {
+      const o = tableMeta.toObject(row)
+      // console.log(`${o._measurement} , ${o._value}, ${o._field}`)
+
+      x.push(o._value);
+      time_x.push(o._time);
+      // console.log(
+      //   `${o._time} ${o._measurement} : ${o._field}=${o._value}`
+      // )
+
+    },
+    error(error) {
+      console.error(error)
+      console.log('\nFinished ERROR')
+    },
+    complete() {
+      console.log(fluxQuery);
+      res.send({ "time": time_x, "vals": x });
+      console.log('\nFinished SUCCESS')
+    },
+  });
+
 }
 
-const add_check = (req,res)=>{
-    var check_meas = req.body.measurement;
-    var check_val = parseInt(req.body.value);
+const add_check = (req, res) => {
+  var check_meas = req.body.measurement;
+  var check_val = parseInt(req.body.value);
 
-    checks[check_meas] = check_val;
-    var x = {status: "New Threshold check Added"};
-    res.send(x);
+  checks[check_meas] = check_val;
+  var x = { status: "New Threshold check Added" };
+  res.send(x);
 }
+
 
 module.exports = {
-    graph_data,
-    add_check,
+  graph_data,
+  add_check,
 }
--- Backend/routing.js
+++ Backend/routing.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const dbFunctions = require('./dbFunctions')
 
-router.get('/graph/:metric/:value/:time_start/:time_end', dbFunctions.graph_data)
+router.get('/graph/:process/:metric/:time_start/:time_end', dbFunctions.graph_data)
 router.post('/threshold_check/', dbFunctions.add_check)
 
 module.exports = router;
