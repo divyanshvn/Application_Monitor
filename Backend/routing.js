c9562eb7b72d0a575ec46e4e2ee1c34b6bcb94f5
divyanshvn <190050035@iitb.ac.in>
2022-04-29 18:49:00 +0530

    bug fix and revert

--- Backend/routing.js
+++ Backend/routing.js
@@ -4,8 +4,9 @@ const dbFunctions = require('./dbFunctions')
 const UserFunctions = require('./userFunctions')
 
 router.get('/graph/:process/:metric/:time_start/:time_end', dbFunctions.graph_data)
-router.post('/threshold_check/', dbFunctions.add_check)
+// router.post('/add_check/', dbFunctions.add_check)
 router.post('/user/register/', UserFunctions.register_user)
 router.post('/user/login', UserFunctions.userAuth)
+// router.post('/check/', dbFunctions.check_data)
 
 module.exports = router;
